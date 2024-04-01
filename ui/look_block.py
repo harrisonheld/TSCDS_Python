800ede0616ec05e9b516fbb8079ae713b520debd
Harrison Held <harrydheld@gmail.com>
2024-04-01 13:22:48 -0500

    look block shows health status

--- ui/look_block.py
+++ ui/look_block.py
@@ -21,28 +21,28 @@ class LookBlock:
             hp_percent = actor.fighter.hp / actor.fighter.max_hp
 
             status_str: str
-            status_color : Tuple[int, int, int]
+            status_color: Tuple[int, int, int]
 
             if hp_percent >= 1.0:
                 status_str = "Perfect"
-                status_color = color.white
+                status_color = color.status_perfect
             elif hp_percent >= 0.75:
                 status_str = "Fine"
-                status_color = (0, 127, 0)
+                status_color = color.status_fine
             elif hp_percent >= 0.3:
                 status_str = "Hurt"
-                status_color = (127, 127, 0)
+                status_color = color.status_hurt
             elif hp_percent >= 0.1:
                 status_str = "Wounded"
-                status_color = (127, 0, 0)
+                status_color = color.status_wounded
             elif hp_percent > 0:
                 status_str = "Critically Wounded"
-                status_color = color.red
+                status_color = color.status_critically_wounded
             else:
                 status_str = "Dead"
-                status_color = (127, 127, 127)
+                status_color = color.status_dead
 
-            console.print(x + 1, y+height-2, status_str, status_color)
+            console.print(x + 1, y+height-1, status_str, status_color)
 
         if acc_y > height-2:
             console.print(x+width-4, y+height-1, "...")
