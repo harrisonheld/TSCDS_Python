766d9cfa9e9fe02c1811f2729b6d6d1ef09b551d
Harrison Held <harrydheld@gmail.com>
2024-05-05 17:34:51 -0500

    Fume Knight just got cooler

--- components/ai/fumeknight_ai.py
+++ components/ai/fumeknight_ai.py
@@ -4,7 +4,9 @@ from typing import List, Tuple
 
 import color
 import entity_factories
+from actions.bump_action import BumpAction
 from actions.spawn_action import SpawnAction
+from actions.wait_action import WaitAction
 from components.ai.ai_base import AIBase
 from entity import Actor
 
@@ -13,11 +15,41 @@ class FumeKnightAI(AIBase):
     def __init__(self, entity: Actor):
         super().__init__(entity)
         self.path: List[Tuple[int, int]] = []
+        self.gas_period = 10
+        self.gas_cooldown = 5
+        self.gas_duration = 3  # for how many turns to emit gas
 
     def perform(self) -> None:
         target = self.engine.player
         dx = target.x - self.entity.x
         dy = target.y - self.entity.y
+        distance = max(abs(dx), abs(dy))
 
-        SpawnAction(self.entity, entity_factories.gas, self.entity.x, self.entity.y).perform()
-        self.engine.message_log.add_message("The Fume Knight releases a cloud of toxic gas!", color.yellow)
+        self.gas_cooldown -= 1
+        if self.gas_duration == self.gas_cooldown:
+            self.engine.message_log.add_message("The Fume Knight impales his ventilated sword into the ground.", color.yellow)
+            WaitAction(self.entity).perform()
+            return
+        if self.gas_cooldown < self.gas_duration:
+            if self.gas_cooldown == self.gas_duration - 1:
+                self.engine.message_log.add_message("The Fume Knight's sword begins to release toxic gas from the earth.", color.yellow)
+            # else:
+            #     self.engine.message_log.add_message("The Fume Knight's sword continues to release residual vapor.", color.yellow)
+            if self.gas_cooldown == 0:
+                self.gas_cooldown = self.gas_period
+            SpawnAction(self.entity, entity_factories.gas, self.entity.x, self.entity.y).perform()
+
+        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+            self.path = self.get_path(target.x, target.y)
+
+        if self.path:
+            old_x, old_y = self.entity.x, self.entity.y
+            dest_x, dest_y = self.path.pop(0)
+            BumpAction(
+                self.entity,
+                dest_x - old_x,
+                dest_y - old_y,
+            ).perform()
+            return
+
+        WaitAction(self.entity).perform()
