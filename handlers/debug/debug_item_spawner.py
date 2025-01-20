7257ddbb53ff3261d71e4410916a49b4303fb55a
Harrison Held <harrydheld@gmail.com>
2025-01-20 13:54:12 -0600

    .

---
+++ handlers/debug/debug_item_spawner.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+import tcod
+
+from entity import Item
+from handlers.action_or_handler import ActionOrHandler
+from handlers.event_handler import EventHandler
+from handlers.item_picker import ItemPicker
+from handlers.main_game_event_handler import MainGameEventHandler
+import color
+import entity_factories
+import keys
+
+
+class DebugItemSpawner(ItemPicker):
+
+    TITLE = "(Debug) Debug Item Spawner"
+
+    def __init__(self, engine):
+        all_entities = [getattr(entity_factories, name) for name in dir(entity_factories) if not name.startswith("_")]
+        self.all_items = [x for x in all_entities if isinstance(x, entity_factories.Item)]
+
+        super().__init__(engine)
+
+    def on_item_selected(self, item: Item) -> Optional[ActionOrHandler]:
+        item.spawn(self.engine.game_map, self.engine.player.x, self.engine.player.y)
+        self.engine.message_log.add_message(f"Spawned '{item.name}'.", color.debug)
+        return None
+
+    def generate_items(self) -> List[Item]:
+        # no need to do any update logic, so juts return what we already have
+        return self.all_items
