7a5f1c23e53aca84094bad52066270dc785f2385
Harrison Held <harrydheld@gmail.com>
2025-01-26 21:00:59 -0600

    fixed borken test

--- tests/test_random_tables.py
+++ tests/test_random_tables.py
@@ -19,23 +19,26 @@ class TestRandomTable(unittest.TestCase):
                 ("orange", 1),
             ]
         )
-
-        stuff = RandomTable[str](
+        nested = RandomTable[str](
             [
                 (games, 1),
-                (fruits, 1),
-                ("cake", 1),
+                (fruits, 2),
+                ("cake", 3),
             ]
         )
 
         roll_count = 100_000
-        results = [stuff.roll() for _ in range(roll_count)]
+        results = [nested.roll() for _ in range(roll_count)]
         counts = Counter(results)
 
         cake_frequency = counts["cake"] / roll_count
-        expected_cake_probability = 1 / 3
+        expected_cake_probability = 1 / 2
         self.assertAlmostEqual(cake_frequency, expected_cake_probability, delta=0.01)
 
+        apple_frequency = counts["apple"] / roll_count
+        expected_apple_probability = 1 / 9
+        self.assertAlmostEqual(apple_frequency, expected_apple_probability, delta=0.01)
+
         fortnite_frequency = counts["fortnite"] / roll_count
-        expected_fortnite_probability = 1 / 9
+        expected_fortnite_probability = 1 / 18
         self.assertAlmostEqual(fortnite_frequency, expected_fortnite_probability, delta=0.01)
