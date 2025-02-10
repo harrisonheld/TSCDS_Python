b2446b5497af2c2d3a1cf314e0ad58ae21c6b87e
Harrison Held <harrydheld@gmail.com>
2025-02-09 20:54:13 -0600

    fixed glass sphere glyph

--- blueprints/items.py
+++ blueprints/items.py
@@ -240,4 +240,4 @@ phlogistonated_chicken_heart: Item = Item(
     name="phlogistonated chicken heart",
     description="A cracked, grey chicken heart. Though dry and brittle, it refuses to crumble. An early experiment, the alchemists believed that combustion could sustain a heart indefinitely. Others believe man's heart is better left unkindled.",
 )
-glass_sphere: Item = Item(char="Ø", color=color.white, name="glass sphere")
+glass_sphere: Item = Item(char="•", color=color.white, name="glass sphere")
